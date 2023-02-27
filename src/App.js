a3ef6b6a877b1387c83769ef58110d608adb9bad
gunnarverhulst <verhulstg@hotmail.com>
2023-02-27 07:01:06 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import Setup from './tutorial/2-useEffect/setup/1-useEffect-basics';
+
+
+function App() {
+  return (
+    <div className='container'>
+      <Setup />
+    </div>
+  )
+}
+
+export default App
---
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+
+const UseStateBasics = () => {
+  
+  // const value = useState(1)[0];
+  // const handler = useState(1)[1];
+  // console.log(value, handler);
+  const [text,setText] = useState('random title');
+
+  const handleClick = () => {
+    if(text === 'random title'){
+      setText('Hello World');
+    } else {
+      setText('random title');
+    }
+    
+  }
+
+  return <React.Fragment>
+    <h1>{text}</h1>
+    <button className='btn' onClick={handleClick}>
+      Change title
+    </button>
+  </React.Fragment>;
+};
+
+export default UseStateBasics;
